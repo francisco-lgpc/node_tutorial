8f8910ba8d18a8e4e28b5ce23438631c45ecf458
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-06-14 17:51:13 +0100

    writable streams

---
+++ writable_streams.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/read_me.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/write_me.txt');
+
+
+myReadStream.on('data', function(chunk){
+  console.log('new chunk received');
+  myWriteStream.write(chunk);
+});
